14d31e06cd74cfe57015ea76aef8e143c9596b53
Guillaume Darbonne <guidouil@gmail.com>
2023-03-11 07:01:36 +0100

    Profile fix

--- imports/ui/pages/search/search.js
+++ imports/ui/pages/search/search.js
@@ -1,1 +1,25 @@
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+import { $ } from 'meteor/jquery';
+
 import './search.html';
+
+Template.search.onCreated(() => {
+  const instance = Template.instance();
+  instance.happenings = new ReactiveVar();
+});
+
+Template.search.helpers({
+  happenings() {
+    return Template.instance().happenings.get();
+  },
+});
+
+Template.search.events({
+  'submit #searchForm'(event, templateInstance) {
+    event.preventDefault();
+    const search = $('#search').val();
+    console.log(search);
+  },
+});
