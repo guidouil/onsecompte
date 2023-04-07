a33c072735da4747568273a7fdf51c73dea4999a
Guillaume Darbonne <guidouil@gmail.com>
2023-04-07 19:48:35 +0200

    Correction du five star pour maj au changement

--- imports/ui/components/fiveStars/fiveStars.js
+++ imports/ui/components/fiveStars/fiveStars.js
@@ -6,28 +6,35 @@ Template.fiveStars.onCreated(() => {
   const instance = Template.instance();
   instance.rating = new ReactiveVar();
   instance.isReadOnly = new ReactiveVar();
-  const { rating, isReadOnly } = instance.data;
-  if (rating) {
-    instance.rating.set(rating);
-  }
-  if (isReadOnly) {
-    instance.isReadOnly.set(isReadOnly);
-  }
+});
+
+Template.fiveStars.onRendered(() => {
+  const instance = Template.instance();
+  instance.autorun(() => {
+    const { rating, isReadOnly } = instance.data;
+    if (rating) {
+      instance.rating.set(rating);
+    }
+    if (isReadOnly) {
+      instance.isReadOnly.set(isReadOnly);
+    }
+  });
 });
 
 Template.fiveStars.helpers({
   starsCount() {
     return [1, 2, 3, 4, 5];
   },
-  isSelected(starNumber) {
-    const rating = Template.instance().rating.get();
+  isSelected(starNumber, rating) {
+    if (rating === undefined) {
+      rating = Template.instance().rating.get();
+    }
     if (starNumber <= rating) {
       return true;
     }
     return false;
   },
-  isHalf(starNumber) {
-    const rating = Template.instance().rating.get();
+  isHalf(starNumber, rating) {
     if (starNumber - 0.5 <= rating) {
       return true;
     }
